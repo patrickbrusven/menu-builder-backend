8197c3befbce9d7d29072ce2076be6d11d31904c
Patrick Brusven <pjbrusven@gmail.com>
2021-07-06 22:09:47 -0700

    middleware move below port setup

--- server/index.js
+++ server/index.js
@@ -40,6 +40,15 @@ app.use('/api/menus', menusRoute);
 app.use('/api/users', usersRoute);
 app.use('/api', authRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  // static folder
+  app.use(express.static(__dirname + '/public/'));
+  // handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 5000;
+
 
 // Error handling middle ware
 app.use((req, res, next) => {
@@ -57,13 +66,13 @@ app.use((err, req, res, next) => {
   });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  // static folder
-  app.use(express.static(__dirname + '/public/'));
-  // handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
-
-const port = process.env.PORT || 5000;
+// if (process.env.NODE_ENV === 'production') {
+//   // static folder
+//   app.use(express.static(__dirname + '/public/'));
+//   // handle SPA
+//   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+// }
+//
+// const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
