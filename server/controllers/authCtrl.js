87e2b1f872bb22be4d071e04d15198548825e858
Patrick Brusven <pjbrusven@gmail.com>
2021-08-11 13:18:54 -0700

    added bycrypt.js dependencie, password hashed before saving to database.

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel.js');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
 
 function asyncHandler(cb){
   return async (req, res, next)=>{
@@ -20,35 +21,31 @@ module.exports = {
   }),
 
   registerUser: asyncHandler( async(req, res, next) => {
-    // check if user email already exists
+
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
-    const newUser = new User(req.body);
-    newUser.token = 'thisIsAToken';
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword
+    });
+
+    const token = await jwt.sign({_id: newUser._id}, process.env.TOKEN_SECRET)
+    newUser.token = token;
     const user = await newUser.save()
     res.status(201).header('auth-token', user.token).json(user);
   }),
-
-  // loginUser: asyncHandler( async(req, res, next) => {
-  //   const user = await User.findOne({ email: req.body.email });
-  //   if(!user) return res.status(400).send("Email dosen't exist");
-  //
-  //   const clientPassword = req.body.password;
-  //   if (user.password != clientPassword) {
-  //     return res.status(400).send('Invalid password')
-  //   }
-  //   user.token = 'thisIsAToken';
-  //   await user.save();
-  //   res.status(200).header('auth-token', user.token).json(user);
-  // }),
 
   loginUser: asyncHandler( async(req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
     if(!user) return res.status(400).send("Email dosen't exist");
 
-    const clientPassword = req.body.password;
-    if (user.password != clientPassword) {
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
       return res.status(400).send('Invalid password')
     }
 
